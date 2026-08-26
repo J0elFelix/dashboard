c47c57e111b31356f8c45f77afe0ee5c3d0ada16
Felix, Joel-MGB <joel.felix@mgb.ch>
2026-08-26 13:07:01 +0200

    deleted menuplan and added it to new folder

--- generate_menuplan.bat
+++ generate_menuplan.bat
@@ -2,6 +2,6 @@ cd C:\_Projects\Projekt\dashboard
 
 python download.py
 
-copy "C:\_Projects\Projekt\dashboard\menuplan-1.pdf" "C:\_Projects\Projekt\dashboard\pdf\"
+copy "C:\_Projects\Projekt\dashboard\menuplan-1.pdf" "C:\_Projects\Projekt\dashboard\generated\"
 
 del "C:\_Projects\Projekt\dashboard\menuplan-1.pdf"
