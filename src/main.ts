c02dd6b8ad36527a5c5ba973b5e4d175cabe7dbc
Felix, Joel-MGB <joel.felix@mgb.ch>
2026-08-27 13:22:32 +0200

    added imports

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-import './departure'
-import './news'
-import './graphClient'
+// Cards
+import './cards/departure'
+import './cards/news'
+import './cards/graphClient'
+
+// Styles
+import './styles/departure.scss'
+import './styles/graphClient.scss'
+import './styles/menuplan.scss'
+import './styles/news.scss'
+import './styles/styles.scss'
