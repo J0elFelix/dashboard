e6436c3275a802d123320fad54159c5f4a226320
Felix, Joel-MGB <joel.felix@mgb.ch>
2026-08-27 09:38:04 +0200

    finished calendar

--- src/graphClient.ts
+++ src/graphClient.ts
@@ -14,8 +14,16 @@ async function calendar(): Promise<void> {
     return;
   }
 
+  const monday = new Date();
+  // Montag beginn und Sonntag ende
+  monday.setDate(monday.getDate() - ((monday.getDay() + 6) % 7));
+  monday.setHours(0, 0, 0 ,0);
+
+  const nextMonday = new Date(monday);
+  nextMonday.setDate(nextMonday.getDate() + 7);
+
   try {
-    const response = await fetch("https://graph.microsoft.com/v1.0/me/calendarview?startdatetime=2026-08-24T10:52:09.500Z&enddatetime=2026-08-28T10:52:09.500Z",
+    const response = await fetch(`https://graph.microsoft.com/v1.0/me/calendarview?startdatetime=${monday.toISOString()}&enddatetime=${nextMonday.toISOString()}`,
       {
         headers: {
           Authorization: `Bearer ${GRAPH_TOKEN}`,
@@ -34,19 +42,37 @@ async function calendar(): Promise<void> {
 
     container.innerHTML = (result.value ?? [])
       .map((event: any) => {
-        const start = new Date(event.start.dateTime).toLocaleString("de-CH");
-        const end = new Date(event.end.dateTime).toLocaleString("de-CH");
+        const allDay = event.isAllDay === true;
+        const start = new Date(event.start.dateTime);
+        const end = new Date(event.end.dateTime);
+        const location = event.location?.displayName || "";
+        const time = allDay ? `<span>Ganztägig</span>` : `
+
+        <span>${start.toLocaleTimeString("de-CH", { hour: "2-digit", minute: "2-digit", })}</span>
+        -
+        <span>${end.toLocaleTimeString("de-CH", { hour: "2-digit", minute: "2-digit", })}</span>
+        `;
+
+        const startDate = start.toLocaleDateString("de-CH", { day: "2-digit", month: "2-digit"});
+        const endDate = end.toLocaleDateString("de-CH", { day: "2-digit" , month: "2-digit"});
+        const formattedDate = startDate === endDate ? startDate : `${startDate} - ${endDate}`;
 
         return `
           <article class="calendar-item">
-            <h3>${event.subject || "Ohne Titel"}</h3>
-            <p>${start} – ${end}</p>
+            <div class="calendar-time">
+              <div>${formattedDate}</div>
+              <div>${time}</div>
+            </div>
+            <div class="calendar-title">
+              <h3>${event.subject || "Kein Titel"}</h3>
+              ${location ? `<p>${location}</p>` : ""}
+            </div>            
           </article>
         `;
       })
       .join("");
   } catch (error) {
-    console.error("Ein Fehler ist aufgetreten:", error);    
+    console.error("Ein Fehler ist aufgetreten:", error);
   }
 }
 
