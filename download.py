ff56a820d8f34b0c91cdff341d6acd6133979d77
Felix, Joel-MGB <joel.felix@mgb.ch>
2026-09-02 08:39:31 +0200

    confert pdf into a png

--- download.py
+++ download.py
@@ -1,6 +1,20 @@
 import urllib.request
+import pymupdf
 
 url = "https://www.restaurant-bzg.ch/dokumente/menueplaene/bzg1/menuplan-1.pdf"
 path = "menuplan-1.pdf"
 
 urllib.request.urlretrieve(url, path)
+
+doc = pymupdf.open("menuplan-1.pdf")
+
+page = doc[0]
+
+zoom = 300 / 72
+matrix = pymupdf.Matrix(zoom, zoom)
+
+pix = page.get_pixmap(matrix=matrix)
+
+pix.save("menuplan-1.png")
+
+doc.close()
--- generate_menuplan.bat
+++ generate_menuplan.bat
@@ -2,6 +2,8 @@ cd C:\_Projects\Projekt\dashboard
 
 python download.py
 
-copy "C:\_Projects\Projekt\dashboard\menuplan-1.pdf" "C:\_Projects\Projekt\dashboard\generated\"
-
 del "C:\_Projects\Projekt\dashboard\menuplan-1.pdf"
+
+copy "C:\_Projects\Projekt\dashboard\menuplan-1.png" "C:\_Projects\Projekt\dashboard\generated\"
+
+del "C:\_Projects\Projekt\dashboard\menuplan-1.png"
